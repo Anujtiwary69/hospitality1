57217a2861d807ed11a9e282597adc5d059f6237
Anuj Tiwary <anujtiwary69@gmail.com>
2024-10-15 17:01:43 +0400

    Merge remote-tracking branch 'github/main'
    # Conflicts:
    #	public/assets/css/style.css

--- src/app/[locale]/components/home/hero.tsx
+++ src/app/[locale]/components/home/hero.tsx
@@ -7,36 +7,36 @@ import styles from './HeroBanner.module.css';
 export default function Hero() {
     const {t} = useTranslation()
     return (
-        <>
-            <div className="video-background-holder">
-                <div className="video-background-overlay"/>
-                <video
-                    playsInline={true}
-                    autoPlay={true}
-                    muted={true}
-                    loop={true}
-                >
-                    <source
-                        src="/assets/images/slider/video.mp4"
-                        type="video/mp4"
-                    />
-
-                </video>
-                <div className="video-background-content container h-100 text-center" style={{top: '40%'}}>
+        <div className="header-Hero">
+            <div className="overlay" />
+            {/* The HTML5 video element that will create the background video on the header */}
+            <video
+                playsInline={true}
+                autoPlay={true}
+                muted={true}
+                loop={true}
+            >
+                <source
+                    src="/assets/images/slider/video.mp4"
+                    type="video/mp4"
+                />
+            </video>
+            {/* The header content */}
+            <div className="container h-100">
+                <div className="d-flex h-100 text-center align-items-center">
                     <div className="hero-content">
                         <h4>{t('home:designed_for_you')}</h4>
                         <h1>{t('home:crafting_experiences')}</h1>
+                        <div className="luxury-button">
+                            <a href="/" className="hero_button">{t('home:discover_more')}</a>
+                        </div>
 
-                    </div>
-
-                    <div className="luxury-button">
-                        <a href="/public">{t('home:discover_more')}</a>
                     </div>
 
                 </div>
             </div>
+        </div>
 
-        </>
 
     );
 }
